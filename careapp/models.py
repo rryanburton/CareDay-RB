0283dfece795940cebf21048a671002da7bb9647
Ryan Burton <rryanburton@gmail.com>
2015-11-02 18:18:43 -0500

    updated migrations

--- careapp/models.py
+++ careapp/models.py
@@ -5,6 +5,22 @@ from django.utils import timezone
 
 
 # Assume 'dailyreport_id' is defined.
+class Child(models.Model):
+    BOY = 'B'
+    GIRL = 'G'
+    GENDER_CHOICES = (
+        (BOY, 'B'),
+        (GIRL, 'G'),
+    )
+    gender = models.CharField(max_length=1,
+                              choices=GENDER_CHOICES,
+                              default=BOY)
+
+    first_name = models.CharField(max_length=10)
+    birthday = models.DateField()
+    parent_name = models.CharField(max_length=10)
+    parent_email = models.EmailField(max_length=254)
+    parent_phone = models.CharField(max_length=12)
 
 
 class DailyReport(models.Model):
@@ -33,21 +49,3 @@ class DailyReport(models.Model):
 
     # def __str__(self):
     #     return ("Name: {}, Date: {}".format(Child.name, date))
-
-
-class Child(models.Model):
-    BOY = 'B'
-    GIRL = 'G'
-    GENDER_CHOICES = (
-        (BOY, 'B'),
-        (GIRL, 'G'),
-    )
-    gender = models.CharField(max_length=1,
-                              choices=GENDER_CHOICES,
-                              default=BOY)
-
-    first_name = models.CharField(max_length=10)
-    birthday = models.DateField()
-    parent_name = models.CharField(max_length=10)
-    parent_email = models.EmailField(max_length=254)
-    parent_phone = models.CharField(max_length=12)
