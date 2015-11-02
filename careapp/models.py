d842eee3dd3d3a4e41f68350e55aa6232d8ab0f5
Ryan Burton <rryanburton@gmail.com>
2015-11-02 16:15:45 -0500

    added Child model

--- careapp/models.py
+++ careapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Child(models.Model):
+    first_name = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10)
+    birthday = models.DateField()
+    parent_name = models.CharField(max_length=10)
+    parent_email = models.EmailField(max_length=254)
+    parent_phone = models.CharField(max_length=12)
