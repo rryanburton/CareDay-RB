638bc8861957fa53db9c18b23fa0ea43a2ea9014
Ryan Burton <rryanburton@gmail.com>
2015-11-02 21:50:19 -0500

    added admin views for child and dailyreport to admin

--- careapp/admin.py
+++ careapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Child, DailyReport
 # Register your models here.
+
+
+class ChildAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'gender', 'birthday',
+                    'parent_name', 'parent_email', 'parent_phone']
+
+
+class DailyReportAdmin(admin.ModelAdmin):
+    list_display = ['id', 'date', 'child', 'arrival_time',
+                    'departure_time', 'mood_am', 'mood_pm']
+
+
+admin.site.register(Child, ChildAdmin)
+admin.site.register(DailyReport, DailyReportAdmin)
