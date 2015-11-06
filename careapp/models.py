1f8e9b9ba1cfba3f0e70e748d82f1e50a987a187
Ryan Burton <rryanburton@gmail.com>
2015-11-06 16:53:06 -0500

    bob and ryan re-engineering database migrations

--- careapp/models.py
+++ careapp/models.py
@@ -17,11 +17,11 @@ class Child(models.Model):
                               choices=GENDER_CHOICES,
                               default=BOY)
 
-    first_name = models.CharField(max_length=10)
-    birthday = models.DateField()
-    parent_name = models.CharField(max_length=10)
-    parent_email = models.EmailField(max_length=254)
-    parent_phone = models.CharField(max_length=12)
+    first_name = models.CharField(max_length=20)
+    birthday = models.DateField(blank=True, null=True)
+    parent_name = models.CharField(max_length=20, blank=True, null=True)
+    parent_email = models.EmailField(max_length=254, blank=True, null=True)
+    parent_phone = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return ("{} : {}".format(self.first_name, self.gender))
@@ -42,8 +42,8 @@ class DailyReport(models.Model):
     )
     date = models.DateField(default=timezone.now)   # default = Year-Mo-Day
     child = models.ForeignKey(Child)   # Assume '_id' will be added to 'child'
-    arrival_time = models.TimeField()
-    departure_time = models.TimeField(null=True)
+    arrival_time = models.TimeField(blank=True, null=True)
+    departure_time = models.TimeField(blank=True, null=True)
     mood_am = models.CharField(max_length=2,
                                choices=MOOD_CHOICE,
                                default=HAPPY)
@@ -57,8 +57,8 @@ class DailyReport(models.Model):
 
 class Diapering(models.Model):
     # assumed diapering_id
-    dailyreport_id = models.ForeignKey(DailyReport)
+    dailyreport = models.ForeignKey(DailyReport)
     time_diaper = models.TimeField()
     num_one = models.BooleanField()  # default is None when empty
     num_two = models.BooleanField()
-    comments = models.CharField(max_length=100)
+    comments = models.CharField(max_length=100, blank=True, null=True)
