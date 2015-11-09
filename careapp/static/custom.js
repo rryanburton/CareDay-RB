4f0b767029c3118dc985b9025c18a62c13eb06a4
drumslayert <ddc@terrymcalhoun.com>
2015-11-09 17:24:06 -0500

    add styls a nd jquery

--- careapp/static/custom.js
+++ careapp/static/custom.js
@@ -1,2 +1,15 @@
 $(document).ready(function() {
-  $("#datepicker").datepicker(); }); 
+  $("#datepicker").datepicker(); });
+
+$(document).ready(function(){
+  $("#report1").click(function(){
+    //make a request
+    $("#report1").load("/diapering/new");
+
+  });
+  $("#report2").click(function(){
+    //make a request
+    $("#report2").load("/sleeping/new");
+
+  });
+});
