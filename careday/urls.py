2ad15030bd75985503d242c48218ffbe7c360199
Ryan Burton <rryanburton@gmail.com>
2015-11-11 11:45:39 -0500

    fixed urls

--- careday/urls.py
+++ careday/urls.py
@@ -19,9 +19,8 @@ from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
 from careapp.views import ChildListView, ChildCreateView, ChildUpdateView, \
     DailyReportListView, DailyReportCreateView, DailyReportUpdateView, \
-    add_child, DiaperingCreateView, SleepingCreateView, EatingCreateView, TerryCreateView
-
-
+    add_child, DiaperingCreateView, SleepingCreateView, EatingCreateView, \
+    TerryCreateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -50,11 +49,13 @@ urlpatterns = [
 
     # url(r'^$', index, name='index'),
     url(r'^calander$', TemplateView.as_view(
-        template_name='careapp/calander.html'), name='calander',),
-    url(r'^diapering/new$', DiaperingCreateView.as_view(), name='diapering',),
-    url(r'^sleeping/new$', SleepingCreateView.as_view(), name='sleeping',),
-    url(r'^eating/new$', EatingCreateView.as_view(), name='eating',),
+        template_name='careapp/calander.html'), name='calander', ),
+    url(r'^diapering/new$', DiaperingCreateView.as_view(), name='diapering', ),
+    url(r'^sleeping/new$', SleepingCreateView.as_view(), name='sleeping', ),
+    url(r'^eating/new$', EatingCreateView.as_view(), name='eating', ),
     url(r'^dailyintake$', TerryCreateView.as_view(),
-        name='daily-report',),
+        name='daily-report', ),
+    url(r'^$', TemplateView.as_view(
+        template_name='careapp/index.html'), name='index',),
 
 ]
