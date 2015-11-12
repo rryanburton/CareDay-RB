aeaadaa88304379bf1fc3a107b8c5581b8a5d50b
drumslayert <ddc@terrymcalhoun.com>
2015-11-11 23:26:41 -0500

    add jquery for daily report page

--- careapp/static/custom.js
+++ careapp/static/custom.js
@@ -2,25 +2,17 @@ $(document).ready(function() {
   $("#datepicker").datepicker(); });
 
 $(document).ready(function(){
-  // $("#activity-moods").click(function(){
-  //   $('#activity-moods').load('dailyreport/update/');
-  //      return false;
-  //   });
-  //   $('#report1').click(function () {
-  //    $('#report1').load('/diapering/new');
-  //       return false;
-  //    });
-  // $("#report2").click(function(){
-  //    $('#report2').load('/sleeping/new');
-  //       return false;
-  //    });
-  // $("#report3").click(function(){
-  //   $('#report3').load('/eating/new');
-  //      return false;
-  //   });
-  $("#diaper").on('click', function(){
-      $("#diaper").addClass('active');
 
+  $("#act").click(function(){
+    $('.activities').addClass('active')
+    .animate({
+    opacity: 0.8,
+    left: "+=50",
+    height: "toggle"
+  }, 700, function() {
+    // Animation complete.
+  });
+  $("#act").hide();
   });
 
 
