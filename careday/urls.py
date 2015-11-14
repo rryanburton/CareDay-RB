dccee75dd3ff7d9f47a5a063a30f37d93c88f7d3
Ryan Burton <rryanburton@gmail.com>
2015-11-14 00:37:17 -0500

    fixed calendar

--- careday/urls.py
+++ careday/urls.py
@@ -49,8 +49,8 @@ urlpatterns = [
         DailyReportUpdateView.as_view(), name='dailyreport-update'),
 
     # url(r'^$', index, name='index'),
-    url(r'^calander$', TemplateView.as_view(
-        template_name='careapp/calander.html'), name='calander', ),
+    url(r'^calendar$', TemplateView.as_view(
+        template_name='careapp/calendar.html'), name='calendar', ),
     url(r'^diapering/new$', DiaperingCreateView.as_view(), name='diapering', ),
     url(r'^sleeping/new$', SleepingCreateView.as_view(), name='sleeping', ),
     url(r'^eating/new$', EatingCreateView.as_view(), name='eating', ),
